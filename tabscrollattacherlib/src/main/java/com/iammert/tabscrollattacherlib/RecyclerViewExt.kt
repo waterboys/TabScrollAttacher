06eea95138ef118785353b4e33578b400be226d0
necati <necatisozer@gmail.com>
2021-05-12 10:32:02 +0300

    Use smoothSnapToPosition

--- tabscrollattacherlib/src/main/java/com/iammert/tabscrollattacherlib/RecyclerViewExt.kt
+++ tabscrollattacherlib/src/main/java/com/iammert/tabscrollattacherlib/RecyclerViewExt.kt
@@ -1,17 +1,18 @@
 package com.iammert.tabscrollattacherlib
 
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.LinearSmoothScroller
 import androidx.recyclerview.widget.RecyclerView
 
 internal fun RecyclerView.scrollToPosition(position: Int, scrollMethod: ScrollMethod) {
     when (scrollMethod) {
         is ScrollMethod.Direct -> scrollToPosition(position)
-        is ScrollMethod.Smooth -> smoothScrollToPosition(position)
-        is ScrollMethod.LimitedSmooth -> smoothScrollToPosition(position, scrollMethod.limit)
+        is ScrollMethod.Smooth -> smoothSnapToPosition(position)
+        is ScrollMethod.LimitedSmooth -> smoothSnapToPosition(position, scrollMethod.limit)
     }
 }
 
-private fun RecyclerView.smoothScrollToPosition(position: Int, scrollLimit: Int) {
+private fun RecyclerView.smoothSnapToPosition(position: Int, scrollLimit: Int) {
     layoutManager?.apply {
         when (this) {
             is LinearLayoutManager -> {
@@ -24,10 +25,19 @@ private fun RecyclerView.smoothScrollToPosition(position: Int, scrollLimit: Int)
                 }
                 if (anchorItem != topItem) scrollToPosition(anchorItem)
                 post {
-                    smoothScrollToPosition(position)
+                    smoothSnapToPosition(position)
                 }
             }
-            else -> smoothScrollToPosition(position)
+            else -> smoothSnapToPosition(position)
         }
     }
+}
+
+private fun RecyclerView.smoothSnapToPosition(position: Int) {
+    val smoothScroller = object : LinearSmoothScroller(context) {
+        override fun getVerticalSnapPreference(): Int = SNAP_TO_START
+        override fun getHorizontalSnapPreference(): Int = SNAP_TO_START
+    }
+    smoothScroller.targetPosition = position
+    layoutManager?.startSmoothScroll(smoothScroller)
 }
